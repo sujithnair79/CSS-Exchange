f465b8b6527e7eda02f693e30abd6c4cbb4fdf64
Lukas Sassl <lusassl@microsoft.com>
2023-03-29 15:19:55 +0200

    Remove AzureAD module dependencies

---
+++ Shared/AzureFunctions/Get-AzureAccessTokenViaAzAccounts.ps1
@@ -0,0 +1,65 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Get-AzureAccessTokenViaAzAccounts {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$AzureEnvironmentName = "AzureCloud",
+
+        [Parameter(Mandatory = $false)]
+        [string]$GraphApiUrl = "https://graph.microsoft.com"
+    )
+
+    <#
+        This function is used to get an access token for the Azure Graph API by using the Az.Accounts module.
+    #>
+
+    begin {
+        Write-Verbose "Calling $($MyInvocation.MyCommand)"
+    }
+    process {
+        try {
+            if ($PSVersionTable.PSVersion -lt 5.1.0.0) {
+                Write-Host "Az.Accounts module requires PowerShell version 5.1 or higher. You're running PowerShell $($PSVersionTable.PSVersion)" -ForegroundColor Red
+                return
+            }
+
+            Import-Module "Az.Accounts" -ErrorAction Stop
+            Write-Host "Prompting user for authentication, please minimize this window if you do not see an authorization prompt as it may be in the background"
+            [void](Connect-AzAccount -Environment $AzureEnvironmentName -ErrorAction Stop)
+            $azContext = Get-AzContext -ErrorAction Stop
+
+            if ($null -ne $azContext) {
+                # Parameters for the Authenticate method are:
+                # Account, Environment, Tenant, PromptBehavior, TokenCache, Credential, GraphEndpoint
+                # https://learn.microsoft.com/dotnet/api/microsoft.azure.commands.common.authentication.factories.authenticationfactory.authenticate
+                $azAuth = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate(
+                    $azContext.Account,
+                    $azContext.Environment,
+                    "$($azContext.Tenant.Id)",
+                    $null,
+                    "Never",
+                    $null,
+                    $GraphApiUrl
+                )
+            } else {
+                Write-Host "Something went wrong while connecting to Azure AD. Please try again." -ForegroundColor Red
+                return
+            }
+        } catch [System.IO.FileNotFoundException] {
+            Write-Host "The Az.Accounts module was not found on this computer. Please install it by running: Install-Module -Name `"Az.Accounts`"" -ForegroundColor Red
+            return
+        } catch {
+            Write-Host "Failed to authenticate with Azure AD. Please check your credentials and try again - Exception:`n$_" -ForegroundColor Red
+            return
+        }
+    }
+    end {
+        return [PSCustomObject]@{
+            AccessToken = $azAuth.AccessToken
+            UserId      = $azAuth.UserId
+            TenantId    = $azContext.Tenant.Id
+        }
+    }
+}
---
+++ Shared/AzureFunctions/Get-CloudServiceEndpoint.ps1
@@ -0,0 +1,63 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Get-CloudServiceEndpoint {
+    [CmdletBinding()]
+    param(
+        [string]$EndpointName
+    )
+
+    <#
+        This shared function is used to get the endpoints for the Azure and Microsoft 365 services.
+        It returns a PSCustomObject with the following properties:
+            GraphApiEndpoint: The endpoint for the Microsoft Graph API
+            ExchangeOnlineEndpoint: The endpoint for Exchange Online
+            AzureADEndpoint: The endpoint for Azure Active Directory
+            EnvironmentName: The name of the Azure environment
+    #>
+
+    begin {
+        Write-Verbose "Calling $($MyInvocation.MyCommand)"
+    }
+    process {
+        # https://learn.microsoft.com/graph/deployments#microsoft-graph-and-graph-explorer-service-root-endpoints
+        switch ($EndpointName) {
+            "Global" {
+                $EnvironmentName = "AzureCloud"
+                $graphApiEndpoint = "https://graph.microsoft.com"
+                $exchangeOnlineEndpoint = "https://outlook.office.com"
+                $AzureADEndpoint = "https://login.microsoftonline.com"
+                break
+            }
+            "USGovernmentL4" {
+                $EnvironmentName = "AzureUSGovernment"
+                $graphApiEndpoint = "https://graph.microsoft.us"
+                $exchangeOnlineEndpoint = "https://outlook.office365.us"
+                $AzureADEndpoint = "https://login.microsoftonline.us"
+                break
+            }
+            "USGovernmentL5" {
+                $EnvironmentName = "AzureUSGovernment"
+                $graphApiEndpoint = "https://dod-graph.microsoft.us"
+                $exchangeOnlineEndpoint = "https://outlook.office365.us"
+                $AzureADEndpoint = "https://login.microsoftonline.us"
+                break
+            }
+            "ChinaCloud" {
+                $EnvironmentName = "AzureChinaCloud"
+                $graphApiEndpoint = "https://microsoftgraph.chinacloudapi.cn"
+                $exchangeOnlineEndpoint = "https://outlook.office365.cn"
+                $AzureADEndpoint = "https://login.chinacloudapi.cn"
+                break
+            }
+        }
+    }
+    end {
+        return [PSCustomObject]@{
+            EnvironmentName        = $EnvironmentName
+            GraphApiEndpoint       = $graphApiEndpoint
+            ExchangeOnlineEndpoint = $exchangeOnlineEndpoint
+            AzureADEndpoint        = $AzureADEndpoint
+        }
+    }
+}
